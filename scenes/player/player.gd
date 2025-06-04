11fb872b2ab2acdeeb59e603792e5a86b9ec5e0b
daniel <daniel.blindr@gmail.com>
2025-06-04 17:20:57 +0300

    Refactor pipes

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,8 +3,9 @@ extends CharacterBody2D
 @export_range(1.0, 300.0, 1.0) var jump_velocity: float = 160.0
 @export_range(10.0, 1000.0, 1.0) var gravity: float = 500.0
 
-func _physics_process(delta):    
+func _physics_process(delta):
     velocity.y += gravity * delta
+    # TODO: Variable jump height
     if Input.is_action_just_pressed("jump"):
-        velocity.y = -jump_velocity
+        velocity.y = - jump_velocity
     move_and_slide()
